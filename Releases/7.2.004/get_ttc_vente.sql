a7e9235387c7511f2965e9c5baf371f5c30978f5
Gouchere Yves <gouchereyves@gmail.com>
2024-07-27 09:19:31 +0200

    Release 7.2.08

--- Releases/7.2.004/get_ttc_vente.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION public.get_ttc_vente(id_ bigint)
- RETURNS double precision
- LANGUAGE plpgsql
-AS $function$
-DECLARE	
-BEGIN
-	RETURN get_ttc_vente(id_, TRUE);
-END;$function$
-;
--- Releases/7.2.007/Insert_Page.sql
+++
@@ -1,2 +0,0 @@
-SELECT insert_droit('gescom_hist_pr', 'Page historique PR', 
-	(SELECT id FROM yvs_module WHERE reference = 'com_'), 16, 'A,B,C','P');	
