4819661d8da8303c38c4c21eb7815bcc9de30ae0
Yves G <gouchereyves@gmail.com>
2024-07-29 09:46:21 +0200

    Migration- Clean files

--- Database/temp-48/alter_column_meg_20_01_2022.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE yvs_com_lot_reception ADD COLUMN article bigint;
-ALTER TABLE yvs_com_lot_reception
-  ADD CONSTRAINT yvs_com_lot_reception_article_fkey FOREIGN KEY (article)
-      REFERENCES yvs_base_articles (id) MATCH SIMPLE
-      ON UPDATE CASCADE ON DELETE CASCADE;
-	  
-	  
-ALTER TABLE yvs_com_parametre_vente ADD COLUMN give_bonus_in_status character varying default 'R';
--- Database/temp-53/yvs_prod_declaration_production--update.sql
+++
@@ -1,8 +0,0 @@
-DROP TRIGGER IF EXISTS update ON public.yvs_prod_declaration_production;
-
-CREATE TRIGGER update
-    AFTER UPDATE 
-    ON public.yvs_prod_declaration_production
-    FOR EACH ROW
-    WHEN (new.quantite <> old.quantite OR new.statut <> old.statut OR new.conditionnement <> old.conditionnement OR new.cout_production <> old.cout_production OR new.execute_trigger::text = 'OUI'::text)
-    EXECUTE PROCEDURE public.insert_declaration_production();
